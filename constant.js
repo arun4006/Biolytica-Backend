93743c166fc70eb728564425e0a185133d055eb8
arun4006 <aarunkumar334@gmail.com>
2023-04-17 17:38:20 +0530

    file upload and read in s3

---
+++ constant.js
@@ -0,0 +1,7 @@
+export const region='us-east-1';
+export const signatureVersion= 'v4';
+export const encodedType='base64';
+export const fileUploaded= 'file Uploaded';
+export const errorFromS3Bukcet='Error retrieving files from S3 bucket';
+export const successCode=200;
+export const internalServerError=500;
