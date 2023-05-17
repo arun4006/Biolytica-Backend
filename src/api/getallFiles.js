282abc149fe8b86537f412d678d479d5bbc0dbb1
arun4006 <aarunkumar334@gmail.com>
2023-05-17 12:27:00 +0530

    refactoring all services

--- src/api/getallFiles.js
+++ src/api/getallFiles.js
@@ -1,18 +1,23 @@
-const {getFilesUrl}=require('../services/s3/getFilesUrlService');
-const {successResponse,errorResponse}=require('../utils/response');
-const {ENV_CONSTANTS}=require('../constants/env.constants');
-const {ENV_BUCKETCONSTANTS}=require('../constants/env.bucketConstants')
+const { getFilesUrl } = require("../services/s3/getFilesUrlService");
+const { successResponse, errorResponse } = require("../utils/response");
+const { ENV_CONSTANTS } = require("../constants/env.constants");
+const { ENV_BUCKETCONSTANTS } = require("../constants/env.bucketConstants");
+const {getUserTokenInfo}=require('../services/auth/authServices')
 
-exports.handler =  async (event) => {
-    try {
-        const fileResponse=await getFilesUrl();        
-        return successResponse(ENV_CONSTANTS.SUCCESS_CODE,fileResponse);        
-        } 
-        catch (err) {
-          console.log(err);
-          return errorResponse(ENV_CONSTANTS.INTERNALSERVER_ERROR,ENV_BUCKETCONSTANTS.ERROR_FROM_S3BUCKET);                    
-        }
+exports.handler = async (event) => {
+  try {
+    const userTokenInfo = await getUserTokenInfo(event);
+    console.log("userTokenInfo Status:" + userTokenInfo);
+    if (userTokenInfo == "TOKEN_EXPIRED") {
+      return unauthorizedResponse(ENV_CONSTANTS.UNAUTHORIZED, userTokenInfo);
     }
-
-    
-
+    const fileResponse = await getFilesUrl();
+    return successResponse(ENV_CONSTANTS.SUCCESS_CODE, fileResponse);
+  } catch (err) {
+    console.log(err);
+    return errorResponse(
+      ENV_CONSTANTS.INTERNALSERVER_ERROR,
+      ENV_BUCKETCONSTANTS.ERROR_FROM_S3BUCKET
+    );
+  }
+};
